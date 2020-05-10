78efd0fdfb1a537c88429e2446cd0714e0d228a5
MinhHieu <minhhieu0112000@gmail.com>
2020-05-10 14:02:01 +0700

    18DH110631-BuiTheMinhHieu

---
+++ 18DH110631-BuiTheMinhHieu-buoi4/Bai1-buoi4/NhanVien.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_buoi4
+{
+    abstract class NhanVien
+    {
+        private string maNV;
+        private string hoTen;
+
+        protected string MaNV { get => maNV; set => maNV = value; }
+        protected string HoTen { get => hoTen; set => hoTen = value; }
+
+        public NhanVien() { }
+        public NhanVien(string maNV, string hoTen)
+        {
+            this.maNV = maNV;
+            this.hoTen = hoTen;
+        }
+        public abstract double TinhLuong();
+        public virtual string toString()
+        {
+            return "MANV: " + maNV + " - HoTenNV: " + HoTen;
+        }
+    }
+}
